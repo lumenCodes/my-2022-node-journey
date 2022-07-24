198c553096ed6fd565431871f17d6174dd429f12
lumenCodes <lumenluwiz@gmail.com>
2022-07-24 16:53:12 +0100

    Setting up a cli usining minimist

---
+++ cli script/index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+console.log('I am building a CLI')
+
+let cmdArgs = require('minimist')
+
+var argv = cmdArgs(['help', 'jsjdj'],cmdArgs.string)
+    // (process.argv.slice(2), {
+    //     Boolean: ['help', 'isLive'],
+    //     String: ['file']
+    // })
+console.log(cmdArgs)
+
+
+// how do I add arguments to this
+
+/**
+ * To add cmd arguments use  this syntax
+ * 
+ *  node nameOfThefile.js the args
+ * we use minimist to beautify the output and also set rules
+ */
+
+//add help thingy just like git cli
+
+function help() {
+    console.log(` mycliName
+    
+    This is how to use myCliName
+
+    --help              description of the help
+    example
+
+    --action or text    this is used to calculate sum
+
+
+    `)
+}
+// help()
