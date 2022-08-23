0045a72315b11389ecae93e59d0b3516547a1dbc
lumenCodes <lumenluwiz@gmail.com>
2022-08-23 19:42:32 +0100

    changed the port

--- bookApp/app.mjs
+++ bookApp/app.mjs
@@ -5,7 +5,7 @@ import { ulid } from 'ulid'
 export const app = express()
 
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8000
 console.log(port)
 
 app.listen(port, (req, res) => {
