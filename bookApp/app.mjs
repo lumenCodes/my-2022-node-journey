562c65e7b9ccaa529055669a20c25410eaaef1b8
lumenCodes <lumenluwiz@gmail.com>
2022-08-23 09:43:06 +0100

    FIxed the display error for Get route

--- bookApp/app.mjs
+++ bookApp/app.mjs
@@ -17,31 +17,26 @@ app.use(express.json())
 
 try {
     
-    app.get('/home', (req, res) => {
-        res.send('<h3>This book app will be used to display the list of books that a user has created</h3>' + db)
-        // db.map()
-        
-    });
 
-    
+    app.get('/home', (req, res) => {
+
+        res.send('<h3>This book app will be used to display the list of books that a user has created</h3>' + db.map((book) => {
+            return [book.name].join(" ")
+        })
+        )
+    });
 
     app.post('/createBook', (req, res) => {
         const index = db.length - 1
-        const newBook = 
+        const newBook =
         {
-            
             // id: index + 1,
             id: ulid(),
             name: req.body.name,
-            createdAt: Date.now()
+            createdAt: new Date()
         }
 
-        // if (!newBook){
-        //     res.send('Please add name of the book.')
-        // }else{
-        //     db.push(newBook)
-        // }
-        // index = db.length - 1
+        
         db.push(newBook)
         res.send(db)
     })
@@ -50,33 +45,19 @@ try {
         let incomingUpdate = req.body
 
         incomingUpdate.name = req.body.name
-        console.log(incomingUpdate.name)
-        incomingUpdate.id =req.body.id
+        incomingUpdate.id = req.body.id
 
         let updateItem = db.filter(((x) => x.id == incomingUpdate.id))
-        console.log(updateItem[1])
-            // db[updateItem][1] = incomingUpdate.name
-            // db[updateItem.id] = incomingUpdate.id
-
-
-        // db[updateItem] = req.body.name
-        
-
-        // res.send('hello')
         res.send(db[updateItem])
 
     })
 
-    app.delete('/deletebook',(req,res)=>{
-        // const deleteitem = db.indexOf(req.body)
-        // const deleteIndex = 1
-        // const removeItem = db.splice((deleteitem, deleteIndex))
-        // console.log(removeItem)
+    app.delete('/deletebook', (req, res) => {
         if (req.body !== Number && req.body == 0) {
             res.send('invalid request')
-        } 
-        const updatedBooks = db.filter((book)=> book.id !== req.body.id )
-       db = updatedBooks
+        }
+        const updatedBooks = db.filter((book) => book.id !== req.body.id)
+        db = updatedBooks
 
         res.send(db)
     })
--- bookApp/route.mjs
+++ bookApp/route.mjs
@@ -1,13 +1,45 @@
-// import express from 'express'
-// import { app } from './app.mjs'
+import {app} from express
 
+app.get('/home', (req, res) => {
 
-// try {
-// app.post('/createBook', (req, res) => {
-//     res.send('Livinus 10x dev')
-//     // console.log(book)
-// })
-// } catch(error){
-//     console.error(error)
-// }
-// // console.log(book)
+    res.send('<h3>This book app will be used to display the list of books that a user has created</h3>' + db.map((book) => {
+        return [book.name].join(" ")
+    })
+    )
+});
+
+app.post('/createBook', (req, res) => {
+    const index = db.length - 1
+    const newBook =
+    {
+        // id: index + 1,
+        id: ulid(),
+        name: req.body.name,
+        createdAt: new Date()
+    }
+
+    
+    db.push(newBook)
+    res.send(db)
+})
+
+app.patch('/updateBook', (req, res) => {
+    let incomingUpdate = req.body
+
+    incomingUpdate.name = req.body.name
+    incomingUpdate.id = req.body.id
+
+    let updateItem = db.filter(((x) => x.id == incomingUpdate.id))
+    res.send(db[updateItem])
+
+})
+
+app.delete('/deletebook', (req, res) => {
+    if (req.body !== Number && req.body == 0) {
+        res.send('invalid request')
+    }
+    const updatedBooks = db.filter((book) => book.id !== req.body.id)
+    db = updatedBooks
+
+    res.send(db)
+})
